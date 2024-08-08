a6ddfc406723567059dc7a1f758e2d5c97102f13
shixiaoming <240635165@qq.com>
2024-08-08 19:09:17 +0800

    Initial commit

---
+++ src/test/java/com/zuoer/homeManager/HomemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuoer.homeManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
